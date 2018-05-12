440bb1a2186d887fe3f5365f2f86b8b9c2ee30a3
Fernanda <Fernanda@PC-Fernanda>
2018-05-12 14:42:03 -0300

    Ajustes na inscrição dos usuários nos tópicos

--- news-core/src/tela/TelaInscricao.java
+++ news-core/src/tela/TelaInscricao.java
@@ -11,6 +11,7 @@ import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
 import news.core.NewsServer;
 import news.core.Topic;
 
